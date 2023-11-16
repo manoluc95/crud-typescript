5e01d3b1eaf9cf6719f6491f760567e2941370e9
emirod19 <35197166@ies9023.net>
2023-11-16 18:08:37 -0300

    se agregan todas las rutas de la home

--- src/routes/home.router.ts
+++ src/routes/home.router.ts
@@ -1,5 +1,9 @@
+import passport from 'passport';
+
 import { BaseRouter } from "./router";
 import { HomeController } from "../controllers/home.controller";
+import { Request, Response } from "express";
+import { Helpers } from "../config/helper";
 
 export class HomeRouter extends BaseRouter<HomeController> {
   constructor() {
@@ -11,5 +15,38 @@ export class HomeRouter extends BaseRouter<HomeController> {
       this.controller.getIndex(req, res)
     });
 
+    this.router.get('/signup', Helpers.isNotLoggedIn, (req, res) => {
+      res.render('auth/signup');
+    });
+
+    this.router.post('/signup', passport.authenticate('local.signup', {
+      successRedirect: '/profile',
+      failureRedirect: '/signup',
+      failureFlash: true
+    }));
+
+    this.router.get('/signin', Helpers.isNotLoggedIn, (req, res) => {
+      res.render('auth/signin');
+    });
+
+    this.router.post('/signin', (req: Request, res: Response, next) => {
+      passport.authenticate('local.signin', {
+        successRedirect: '/profile',
+        failureRedirect: '/signin',
+        failureFlash: true
+      })(req, res, next);
+    });
+
+    // Antes de dirigir a profile verificamos si el usuario esta logeado
+    this.router.get('/profile', Helpers.isLoggedIn, (req, res) => {
+      res.render('auth/profile');
+    })
+
+    this.router.get('/logout', (req, res) => {
+      req.logout(function(err) {
+        req.flash('message', 'Gracias por usar nuestra app');
+        res.redirect('/signin');
+      });
+    });
   }
 }
