ca8a3b084c844091a0a1e55f5820454807d627c2
emirod19 <35197166@ies9023.net>
2023-11-16 18:03:57 -0300

    se agregan archivo helper y passport para realizar el ingreso, egreso y registro de usuario

---
+++ src/config/helper.ts
@@ -0,0 +1,37 @@
+import * as bcrypt from "bcrypt";
+import { Request, Response, NextFunction } from "express";
+
+export class Helpers {
+  constructor() {
+  }
+
+  static isLoggedIn(req: Request, res: Response, next: NextFunction){
+      // Usamos un método de passport que devuelve un boolean si hay un usuario logeado
+      if (req.isAuthenticated()) {
+          return next();
+      }
+      return res.redirect('/signin');
+  };
+
+  static isNotLoggedIn(req: Request, res: Response, next: NextFunction){
+      if (!req.isAuthenticated()) {
+          return next();
+      }
+      return res.redirect('/profile');
+  };
+
+  static async encryptPassword(password: string){
+    const salt = await bcrypt.genSalt(10);
+    return await bcrypt.hash(password, salt);
+  };
+
+  static async matchPassword(password: string, savedPassword: string){
+    try {
+      // Realizamos la comparación de un str comun y uno encriptado
+      // Devolviendo un boolean
+      return await bcrypt.compare(password, savedPassword);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+}
