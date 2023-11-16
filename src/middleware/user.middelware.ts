5d50d971777796e46757715697ad96861b6a75b3
emirod19 <35197166@ies9023.net>
2023-11-16 18:07:54 -0300

    se corrige el nombre de la propiedad provincia del usuario ya que se encontraba en portugues dentro de user.middelware

--- src/middleware/user.middelware.ts
+++ src/middleware/user.middelware.ts
@@ -1,14 +1,14 @@
 import { validate } from "class-validator";
 import { NextFunction, Request, Response } from "express";
 import { UserDTO } from "../dto/user.dto";
-import { HttpResponse } from "../../shared/response/http.response";
+import { HttpResponse } from "../shared/response/http.response";
 
 export class UserMiddleware {
   constructor(
     private readonly httpResponse: HttpResponse = new HttpResponse()
   ) {}
   userValidator(req: Request, res: Response, next: NextFunction) {
-    const { name, lastname, username, email, password, city, province, role } =
+    const { name, lastname, username, email, password, city, state, role } =
       req.body;
 
     const valid = new UserDTO();
@@ -19,7 +19,7 @@ export class UserMiddleware {
     valid.email = email;
     valid.password = password;
     valid.city = city;
-    valid.province = province;
+    valid.state = state;
     valid.role = role;
 
     validate(valid).then((err) => {
